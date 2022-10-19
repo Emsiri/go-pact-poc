aeb4887321099f80a90cd5af8f8f49cb2f72476e
emsiri <emsirikami@gmail.com>
2022-10-20 09:29:46 +1000

    Cleaned up contracts

--- consumer/client/pact/publish.go
+++ consumer/client/pact/publish.go
@@ -20,7 +20,7 @@ func main() {
 
 	fmt.Println("Publishing Pact files to broker", os.Getenv("PACT_DIR"), os.Getenv("PACT_BROKER_URL"))
 	err := p.Publish(types.PublishRequest{
-		PactURLs:        []string{filepath.FromSlash(fmt.Sprintf("%s/gouserservice-goadminservice.json", os.Getenv("PACT_DIR")))},
+		PactURLs:        []string{filepath.FromSlash(fmt.Sprintf("%s/goadminservice-gouserservice.json", os.Getenv("PACT_DIR")))},
 		PactBroker:      fmt.Sprintf("%s://%s", os.Getenv("PACT_BROKER_PROTO"), os.Getenv("PACT_BROKER_URL")),
 		ConsumerVersion: version,
 		Tags:            []string{"master"},
